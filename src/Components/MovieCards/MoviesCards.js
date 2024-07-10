a9efbbfcba41c6092bad661ccdd8f491760109f3
“Reese <reesegreen2014@me.com>
2024-07-10 15:50:45 -0600

    routed MovieCard component

--- src/Components/MovieCards/MoviesCards.js
+++ src/Components/MovieCards/MoviesCards.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './MovieCards.css';
+import { Link } from 'react-router-dom';
 
-function MovieCard({ movie, onClick }) {
-  console.log("movie>>>", movie)
+function MovieCard({ movie }) {
   return (
-    <div className="movie-card" onClick={onClick}>
-      <img src={movie.poster_path} alt={`${movie.title} poster`} className="movie-poster" />
-      <div className="movie-details">
-        <h2>{movie.title}</h2>
+    <Link to={`/movies/${movie.id}`} className="movie-card-link">
+      <div className="movie-card">
+        <img src={movie.poster_path} alt={`${movie.title} poster`} className="movie-poster" />
+        <div className="movie-details">
+          <h2>{movie.title}</h2>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
-
 export default MovieCard;
